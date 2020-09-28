e44e233866479fc72b89e2629788b24754940a4d
gromflomite <35708235+gromflomite@users.noreply.github.com>
2020-09-28 13:51:01 +0200

    A lot of changes.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,66 +1,91 @@
-/* 
-// Console output levels
-console.info("Console - Info");
-console.log("Console - Log");
-console.warn("Console - Warn");
-console.error("Console - Error");
-console.debug("Console - Debug");
-*/
+document.addEventListener('DOMContentLoaded', function () {
 
-// Declaring array of dogs
-const dogList = [{
-        "id": 1,
-        "name": "Rex",
-        "breed": "Terrier"
+    console.info("DOM fully ready");
 
-    },
+    // Materialize initialization
+    M.AutoInit();   
 
-    {
-        "id": 2,
-        "name": "Martin",
-        "breed": "Labrador"
-    },
+    // AJAX first call
+    const endpoint = 'http://localhost:8080/perreraWS/service/perro';
 
-    {
-        "id": 3,
-        "name": "Cash",
-        "breed": "Terrier"
-    },
+    var xhttp = new XMLHttpRequest();
 
-    {
-        "id": 4,
-        "name": "Aldan",
-        "breed": "Boxer"
-    },
+    xhttp.open("GET", endpoint);
+    xhttp.send();
 
-    {
-        "id": 5,
-        "name": "Leon",
-        "breed": "Doberman"
-    }
-];
+    xhttp.onreadystatechange = function () {
 
-document.addEventListener('DOMContentLoaded', function() {
+        console.debug('readyState %s: ', this.readyState);
 
-    let loader = document.getElementById("spinner");
-    loader.style.display = "none";
+        // Hide loading spin
+        let loader = document.getElementById("spinner");
+        loader.style.display = "none";
 
-    console.info("DOM fully ready")
+        if (this.readyState == 4 && this.status == 200) {
+
+            console.debug(this.status, this.responseText);
+
+            const dataFromApi = JSON.parse(this.responseText);
+
+            populateList(dataFromApi); // Call the function to fill the HTML list           
+
+        }
+    }; // End AJAX first call
+});
+
+// Get the order values selected by the user
+function getOrderValues() {
+
+    const orderBy = document.getElementById('orderBy').value;
+    const orderType = (document.getElementById('orderType').checked ? 'desc' : 'asc');
+    console.debug('Order values from HTML - Order by: ' + orderBy + ' - Order type: ' + orderType);
+
+    const endpointWithParameters = `http://localhost:8080/perreraWS/service/perro?orderBy=${orderType}&campo=${orderBy}`
+    console.debug(endpointWithParameters);
+
+    // AJAX call with HTML values
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.open("GET", endpointWithParameters);
+    xhttp.send();
+
+    xhttp.onreadystatechange = function () {
+
+        console.debug('readyState %s: ', this.readyState);
+
+        // Hide loading spin
+        let loader = document.getElementById("spinner");
+        loader.style.display = "none";
+
+        if (this.readyState == 4 && this.status == 200) {
+
+            console.debug(this.status, this.responseText);
+
+            const dataFromApi = JSON.parse(this.responseText);
+
+            populateList(dataFromApi); // Call the function to fill the HTML list           
+
+        }
+
+    }; // End AJAX call with HTML values
+};
+
+// Populate HTML list
+function populateList(dataFromApi) {
 
     let eldogList = document.getElementById("dog-list");
     eldogList.innerHTML = "";
     console.debug("List emptied");
 
-    dogList.forEach(dog => {
+    dataFromApi.forEach(ApiItem => {
 
         eldogList.innerHTML +=
-
             `<li class="collection-item avatar">
         <img src="img/logo_dog.png" alt="" class="circle">
-        <span id="dog-name" class="title">${dog.name}</span> <span>(Id: ${dog.id})</span>
-        <p>${dog.breed}</p>
+        <span id="dog-name" class="title">${ApiItem.nombre}</span> <span>(Id: ${ApiItem.id})</span>
+        <p>${ApiItem.raza}</p>
         </li>`
 
     });
 
-});
+}; // End Populate HTML list
